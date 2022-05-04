f15320e86a93cae8f553f70c35a83b4e5e06d5a0
Elena K <alenkaksa81@gmail.com>
2022-05-04 18:03:00 +0300

    Initial commit.

---
+++ src/main/java/com/example/currencyrate/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.currencyrate;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CurrencyRateApplication.class);
+    }
+
+}
