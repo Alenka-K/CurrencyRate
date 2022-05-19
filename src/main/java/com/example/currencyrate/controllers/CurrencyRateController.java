0be67954773b4d09dea57acef95fc13144b72e4b
Elena K <alenkaksa81@gmail.com>
2022-05-19 22:49:11 +0300

    changed the link

--- src/main/java/com/example/currencyrate/controllers/CurrencyRateController.java
+++ src/main/java/com/example/currencyrate/controllers/CurrencyRateController.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 
 
 @RestController
-@RequestMapping(path = "/controller")
+@RequestMapping
 public class CurrencyRateController {
 
     private final NBUService nbuService;
@@ -18,8 +18,12 @@ public class CurrencyRateController {
         this.nbuService = nbuService;
     }
 
+    @GetMapping("/")
+    public String menu() {
+        return "Enter currency code and date (day-month-year). Example: /EUR/01-01-2022)";
+    }
 
-    @GetMapping("/NBU/{currency}/{date}")
+    @GetMapping("/{currency}/{date}")
     public ResponseEntity<?> getRate(@PathVariable("currency") String currency,
                                      @DateTimeFormat(pattern = "dd-MM-yyyy") @PathVariable("date") LocalDate date){
         if (date.isAfter(LocalDate.now())) return ResponseEntity.ok("Date does not exist!");
